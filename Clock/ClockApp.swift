64ef5f42641db07a5b0c9648e34e7e6b50332dce
wingertandrew <wingert.andrew@gmail.com>
2025-09-17 23:31:31 -0400

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Andrew Wingert on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Andrew Wingert on 9/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
